d7c1c75a2dddc433db62ed60cd0f407d0c0aa250
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-05-27 17:56:41 +0530

    Created the server

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const session = require("express-session");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send("Octomail Backend Running");
+});
+
+const PORT = process.env.PORT || 5000; 
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
+    console.log("MongoDB Connected");
+    app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+} );
+}).catch((err)=>{
+    console.log(`Error ${err}`);
+});
+
+
