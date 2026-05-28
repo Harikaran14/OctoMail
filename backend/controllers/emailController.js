95f02eda9051017aa66f34203c000d2022939058
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-05-28 22:00:53 +0530

    Created the emailcontroller to get from mongodb and fetch from gmailapi

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -1,23 +1,44 @@
 const fetchEmails =require("../services/gmailService");
 const {getHeader,extractBody}=require("../utils/emailParser");
 const cleanEmailTexts=require("../utils/cleanEmail");
+const Email=require("../models/Email");
+
 async function getEmails(req,res){
 
     try{
         const accessToken=req.user.accessToken;
         const emails = await fetchEmails(accessToken);
-        const parsedEmails=emails.map(email => {
-            return {
+        const parsedEmails=await Promise.all(
+        
+        emails.map(async email => {
+            const parsedEmail={
+                userId: req.user.googleId,
                 gmailId: email.id,
                 threadId: email.threadId,
                 snippet: email.snippet,
                 subject: getHeader(email.payload.headers,"Subject"),
                 sender:getHeader(email.payload.headers,"From"),
-                body: cleanEmailTexts(extractBody(email.payload))
-
+                body: cleanEmailTexts(extractBody(email.payload)),
+                receivedAt: new Date(getHeader(email.payload.headers,"Date"))
+            
             };
-        });
+            const existingEmail = await Email.findOne({
+                gmailId: parsedEmail.gmailId
+            });
+
+            if (!existingEmail){
+                await Email.create(parsedEmail);
+            }
+            return parsedEmail;
+
+        }
+    )
+    
+        );
         res.json(parsedEmails);
+
+
+     
     }
     catch(err){
         console.log(err);
@@ -27,4 +48,16 @@ async function getEmails(req,res){
     }
 }
 
-module.exports={getEmails};
+async function getStoredEmails(req,res){
+    try{
+        const emails= await Email.find({
+            userId: req.user.googleId
+        }).sort({receivedAt:-1});
+        res.json(emails);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error: `Failed to retrieve email from MongoDB `});
+    }
+}
+module.exports={getEmails,getStoredEmails};
