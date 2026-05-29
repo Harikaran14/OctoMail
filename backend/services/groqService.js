385a35d9511610b4769892c2602ad820027968a7
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-05-29 13:30:54 +0530

    Adding Groq service for backup

---
+++ backend/services/groqService.js
@@ -0,0 +1,26 @@
+const Groq = require("groq-sdk");
+
+const groq= new Groq({
+    apiKey: process.env.GROQ_API_KEY
+});
+
+async function generateWithGroq(prompt) {
+
+    const completion =
+        await groq.chat.completions.create({
+            model: "llama-3.3-70b-versatile",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt
+                }
+            ],
+            temperature: 0.2
+        });
+
+    return completion
+        .choices[0]
+        .message.content;
+}
+
+module.exports = generateWithGroq;
