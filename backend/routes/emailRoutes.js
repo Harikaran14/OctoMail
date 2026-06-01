700065a7df8658bbd0c5114554693f088be610bf
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-06-01 17:11:22 +0530

    adding category wise filtering

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const {getEmails,getStoredEmails} = require("../controllers/emailController");
+const {getEmails,getStoredEmails, getEmailsByCategory} = require("../controllers/emailController");
 const ensureAuth =require("../middleware/authMiddleware");
 
 router.get("/fetch",ensureAuth, getEmails);
 router.get("/stored",ensureAuth,getStoredEmails);
+router.get("/category/:category", ensureAuth,getEmailsByCategory);
 module.exports=router;
 
