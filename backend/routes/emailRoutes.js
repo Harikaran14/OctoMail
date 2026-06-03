ce027b34edb0463cc7584f6e2aa8b23ff4f0affc
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-06-03 14:27:16 +0530

    altering emailroutes.js to check for token and adding token service to use refresh token

--- backend/routes/emailRoutes.js
+++ backend/routes/emailRoutes.js
@@ -1,7 +1,24 @@
 const router = require("express").Router();
 const {getEmails,getStoredEmails, getEmailsByCategory} = require("../controllers/emailController");
 const ensureAuth =require("../middleware/authMiddleware");
+const refreshAccessToken =
+require("../services/tokenService");
 
+router.get(
+    "/test-token",
+    async(req,res)=>{
+
+        const token =
+        await refreshAccessToken(
+            req.user.refreshToken
+        );
+
+        res.json({
+            token
+        });
+
+    }
+);
 router.get("/fetch",ensureAuth, getEmails);
 router.get("/stored",ensureAuth,getStoredEmails);
 router.get("/category/:category", ensureAuth,getEmailsByCategory);
---
+++ backend/services/tokenService.js
@@ -0,0 +1,27 @@
+const axios = require("axios");
+
+async function refreshAccessToken(refreshToken){
+    try {
+    const response = await axios.post(
+        "https://oauth2.googleapis.com/token",
+        {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            refresh_token: refreshToken,
+            grant_type: "refresh_token"
+        }
+    );
+
+    return response.data.access_token;
+
+} catch(err) {
+
+    console.log(
+        err.response?.data || err.message
+    );
+
+    throw err;
+}
+}
+
+module.exports =refreshAccessToken;
