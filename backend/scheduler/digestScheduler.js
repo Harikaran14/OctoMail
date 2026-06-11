02e1e5b82ed3b8e6587d014606d37003e64ee057
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-06-11 16:28:29 +0530

    Creating a scheduler for digests daily basis

---
+++ backend/scheduler/digestScheduler.js
@@ -0,0 +1,51 @@
+require("dotenv").config();
+
+const cron =
+require("node-cron");
+
+const connectDB =
+require("../config/db");
+
+const User =
+require("../models/User");
+
+const digestQueue =
+require("../queues/digestQueue");
+
+(async()=>{
+
+    await connectDB();
+
+    cron.schedule(
+
+        "0 8 * * *",
+
+        async()=>{
+
+            console.log(
+                "Running Digest Scheduler..."
+            );
+
+            const users =
+                await User.find();
+
+            for(const user of users){
+
+                await digestQueue.add(
+
+                    "generate-digest",
+
+                    {
+                        userId:
+                            user._id.toString()
+                    }
+
+                );
+
+            }
+
+        }
+
+    );
+
+})();
