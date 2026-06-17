8fb55184fb5200dd6435cb894149fc1791cee18c
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-06-17 14:30:02 +0530

    Login and logout

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -2,15 +2,27 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 function Sidebar(){
-    const auth= useContext(AuthContext);
+    const {user,setUser,isAuthenticated,setIsAuthenticated}= useContext(AuthContext);
 
     return (
         <div>
             <h2>
                 SideBar
             </h2>
-            <p>{auth.user.name}</p>
-            <p>{auth.user.email}</p>
+            <button onClick={()=>{
+                setUser({name:"Harikaran",email:"Hari@gmail.com"});
+                setIsAuthenticated(true);
+
+            }}>Login</button>
+            <button onClick={()=>{
+                setUser({name:"Demo User",email:"demo@octomail.ai"});
+                setIsAuthenticated(false);
+
+            }}>LogOut</button>
+            <p>{isAuthenticated ? "Logged IN" : "Guest User"}</p>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+            
         </div>
     )
 }
