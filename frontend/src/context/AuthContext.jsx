80da8ef9eb51d62f22869377065aabe840e4f4b1
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-06-19 22:17:10 +0530

    change login logout to simulate real data

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,47 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { getCurrentUser, logoutUser } from "../services/authServices";
 
 export const AuthContext= createContext();
 
 function AuthProvider({children}){
-    const [user,setUser] = useState({
-        name: "Demo User",
-        email: "demo@octomail.ai"
-    });
+    const [user,setUser] = useState(null);
 
     const [isAuthenticated, setIsAuthenticated]= useState(false);
+    
+    const [loading,setLoading]=useState(true);
+
+
+    useEffect(()=>{
+        async function checkAuth(){
+            try {
+                const data = await getCurrentUser();
+                setUser(data.user);
+                setIsAuthenticated(true);
+
+            }
+            catch{
+                setUser(null);
+                setIsAuthenticated(false);
+
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+        checkAuth();
+    },[]);
+
     function login(){
 
-    setUser({
-
-        name:"Harikaran",
-
-        email:"hari@gmail.com"
-
-    });
-
-    setIsAuthenticated(
-        true
-    );
-
+    window.location.href="http://localhost:5000/auth/google"
 }
 
-function logout(){
+async function logout(){
 
-    setUser({
-
-        name:"Demo User",
-
-        email:"demo@octomail.ai"
-
-    });
-
-    setIsAuthenticated(
-        false
-    );
+    try { await logoutUser();}
+    catch(error){console.log(error);}
+    setUser(null);
+    setIsAuthenticated(false);
 
 }
     return (
-        <AuthContext.Provider value = {{user,setUser,isAuthenticated,setIsAuthenticated,login,logout}}>
+        <AuthContext.Provider value = {{user,isAuthenticated,loading, login,logout}}>
 
             {children}
         </AuthContext.Provider>
