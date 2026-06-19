7ae7e62dedc0deb37bab749095411cb9a3ddb95f
Harikaran14 <harikaran2310104@ssn.edu.in>
2026-06-19 22:34:02 +0530

    Refactored for semantic search and searchsimilarEmails

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -69,4 +69,64 @@ async function semanticSearch(req,res){
     }
 }
 
-module.exports={semanticSearch};
+async function getSimilarEmails(req,res){
+    try{
+        const email =await Email.findById(req.params.id);
+        if (!email){
+            return res.status(404).json({message:"Email not found"});
+        }
+        const results = await Email.aggregate([
+            {
+                $vectorSearch: {
+                    index: "vector_index",
+                    path: "embedding",
+                    queryVector: email.embedding,
+                    numCandidates: 100,
+                    limit:5,
+                    filter:{
+                        userId: req.user.googleId
+                    }
+                }
+            },{
+                $project:{
+                    subject:1,
+                    sender:1,
+                    summary:1,
+                    priority:1,
+                    tasks:1,
+                    body:1,
+                    receivedAt:1,
+
+                    score:{
+                        $meta: "vectorSearchScore"
+                    }
+
+
+                }
+            },
+            {
+
+                $match: {
+                    score: {
+                        $gte: 0.7
+                    },
+
+                    _id:{
+                        $ne:email._id
+                    }
+                }
+            }
+        ]);
+
+        console.log("extracted similar emails succesfully")
+        res.json(results);
+    }
+    catch(error){
+        console.log(error.message);
+        res.status(500).json({
+            error: "Failed to get Similar Email "
+        });
+    }
+}
+
+module.exports={semanticSearch, getSimilarEmails};
--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const ensureAuth = require("../middleware/authMiddleware");
-const {semanticSearch} = require("../controllers/searchController");
+const {semanticSearch, getSimilarEmails} = require("../controllers/searchController");
 
 router.post("/semantic",ensureAuth,semanticSearch );
-
+router.get("/:id/similar",ensureAuth,getSimilarEmails);
 module.exports= router;
